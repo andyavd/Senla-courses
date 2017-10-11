c893155c27aa99c2df09531ef010394ea1b5da8a
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-11 19:57:21 +0300

    Update Body.java

--- lesson2/task3/src/com/andyavd/Body.java
+++ lesson2/task3/src/com/andyavd/Body.java
@@ -1,19 +1,4 @@
 package com.andyavd;
 
 public class Body implements IProductPart {
-	
-	private String bodyType;
-	
-	public String getBodyType() {
-        return bodyType;
-    }
-
-    public void setBodyType(String bodyType) {
-        this.bodyType = bodyType;
-    }
-    
-    public Body(String bodyType) {
-		this.bodyType = bodyType;
-		System.out.println("Building Body");
-	}
 }
