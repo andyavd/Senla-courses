c47f5f787bdd8ed68444ad668a2427c2daf8d8bc
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-11 20:00:32 +0300

    Update Engine.java

--- lesson2/task3/src/com/andyavd/Engine.java
+++ lesson2/task3/src/com/andyavd/Engine.java
@@ -1,20 +1,4 @@
 package com.andyavd;
 
 public class Engine implements IProductPart {
-	
-	private int power;
-	
-	public int getPower() {
-        return power;
-    }
-
-    public void setPower(int power) {
-        this.power = power;
-    }
-	
-	public Engine(int power) {
-		this.power = power;
-		System.out.println("Building Engine");
-	}
-
 }
