a0683ff9516f4ecbf0e20a6b51457a87c9bae961
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-11 19:58:45 +0300

    Update Chassis.java

--- lesson2/task3/src/com/andyavd/Chassis.java
+++ lesson2/task3/src/com/andyavd/Chassis.java
@@ -1,19 +1,4 @@
 package com.andyavd;
 
 public class Chassis implements IProductPart {
-	
-	private String drive;
-
-	public String getDrive() {
-        return drive;
-    }
-
-    public void setDrive(String drive) {
-        this.drive = drive;
-    }
-    
-    public Chassis(String drive) {
-		this.drive = drive;
-		System.out.println("Building Chassis");
-	}
 }
