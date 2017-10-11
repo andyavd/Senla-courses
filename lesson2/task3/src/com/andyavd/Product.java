de448dd1c84b812d681b8f544a3489ea6690ba86
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-11 20:03:18 +0300

    Update Product.java

--- lesson2/task3/src/com/andyavd/Product.java
+++ lesson2/task3/src/com/andyavd/Product.java
@@ -1,57 +1,25 @@
 package com.andyavd;
 
 public class Product implements IProduct {
-
 	private Body body;
 	private Chassis chassis;
 	private Engine engine;
 	
 	@Override
 	public void installFirstPart(IProductPart firstProductPart) {
-		System.out.println("Installing the first part");
+		body = (Body) firstProductPart;
+		System.out.println("Installing body");
 	}
 
 	@Override
 	public void installSecondPart(IProductPart secondProductPart) {
-		System.out.println("Installing the second part");
+		chassis = (Chassis) secondProductPart;
+		System.out.println("Installing chassis");
 	}
 
 	@Override
 	public void installThirdPart(IProductPart thirdProductPart) {
-		System.out.println("Installing the third part");
+		engine = (Engine) thirdProductPart;
+		System.out.println("Installing engine");
 	}
-
-	public Body getBody() {
-		return body;
-	}
-
-	public void setBody(Body body) {
-		this.body = body;
-	}
-
-	public Chassis getChassis() {
-		return chassis;
-	}
-
-	public void setChassis(Chassis chassis) {
-		this.chassis = chassis;
-	}
-
-	public Engine getEngine() {
-		return engine;
-	}
-
-	public void setEngine(Engine engine) {
-		this.engine = engine;
-	}
-	
-	public Product(){}
-	
-	public Product(Body body, Chassis chassis, Engine engine) {
-        this.body = body;
-        this.chassis = chassis;
-        this.engine = engine;
-        System.out.println("Creating the Product");
-    }
-
 }
