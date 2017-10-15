3abc102522f54393a3783ed6aabfd829e3e7451d
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-16 02:15:25 +0300

    Add files via upload

---
+++ lesson3/task2/Test.java
@@ -0,0 +1,23 @@
+package task2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int number = 100 + random.nextInt(999 - 100);
+		System.out.println("Randomly generated number is: " + number);
+		
+		int ones = number%10;
+		int tens = ((number-ones)/10)%10;
+		int hundreds = (int) (number/100);
+		
+		int[] array = {hundreds, tens, ones};
+		Arrays.sort(array);
+		
+		System.out.println("Biggest digit in a randomly generated number is: " + array[2]);
+	}
+
+}
