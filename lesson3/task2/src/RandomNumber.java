9c6924551ae06a1329cf3b35f26a79b4cba452e6
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-17 01:56:25 +0300

    Update RandomNumber.java

--- lesson3/task2/src/RandomNumber.java
+++ lesson3/task2/src/RandomNumber.java
@@ -10,15 +10,27 @@ public class RandomNumber {
 	public void createRandomNumber() {
 		number = 100 + random.nextInt(999 - 100);
 		System.out.println("Randomly generated number is: " + number);
-		
+	}
+	
+	public int extractOnes() {
 		int ones = number%10;
-		int tens = ((number-ones)/10)%10;
+		return ones;
+	}
+	
+	public int extractTens() {
+		int tens = ((number-extractOnes())/10)%10;
+		return tens;
+	}
+	
+	public int extractHundreds() {
 		int hundreds = (int) (number/100);
-		
-		int[] array = {hundreds, tens, ones};
-		Arrays.sort(array);
-		
-		System.out.println("Biggest digit in a randomly generated number is: " + array[2]);
+		return hundreds;
+	}
+	
+	public void sortArray() {
+		int[] digitsArray = {extractHundreds(), extractTens(), extractOnes()};
+		Arrays.sort(digitsArray);
+		System.out.println("Biggest digit in a randomly generated number is: " + digitsArray[2]);
 	}
 
 	public RandomNumber() {
