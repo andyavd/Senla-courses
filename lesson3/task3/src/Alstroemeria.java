4dde4e806daddc8f82f5401c3f37403ea014b4c3
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-17 02:14:39 +0300

    Update Alstroemeria.java

--- lesson3/task3/src/Alstroemeria.java
+++ lesson3/task3/src/Alstroemeria.java
@@ -1,16 +1,6 @@
 public class Alstroemeria extends Flower {
-	
-	private int daysOfLife;
 
-	public int getDaysOfLife() {
-		return daysOfLife;
-	}
-	public void setDaysOfLife(int daysOfLife) {
-		this.daysOfLife = daysOfLife;
-	}
-
-	public Alstroemeria (String group, String sort, String color, double price, int daysOfLife) {
+	public Alstroemeria (String group, String sort, String color, double price) {
 		super(group, sort, color, price);
-		this.daysOfLife = daysOfLife;
 	}
 }
