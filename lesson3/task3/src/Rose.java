6eb844890daddc62b3ec30b6b9104e52985d3afe
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-17 02:16:07 +0300

    Update Rose.java

--- lesson3/task3/src/Rose.java
+++ lesson3/task3/src/Rose.java
@@ -1,17 +1,6 @@
 public class Rose extends Flower {
 
-	private int daysOfLife;
-
-	public int getDaysOfLife() {
-		return daysOfLife;
-	}
-
-	public void setDaysOfLife(int daysOfLife) {
-		this.daysOfLife = daysOfLife;
-	}
-
-	public Rose(String group, String sort, String color, double price, int daysOfLife) {
+	public Rose(String group, String sort, String color, double price) {
 		super(group, sort, color, price);
-		this.daysOfLife = daysOfLife;
 	}
 }
