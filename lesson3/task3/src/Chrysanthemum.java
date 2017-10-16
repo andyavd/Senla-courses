d499f8a1577e59e97d49e48ecd95516134d1686b
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-17 02:15:04 +0300

    Update Chrysanthemum.java

--- lesson3/task3/src/Chrysanthemum.java
+++ lesson3/task3/src/Chrysanthemum.java
@@ -1,17 +1,6 @@
 public class Chrysanthemum extends Flower {
 
-	private int daysOfLife;
-
-	public int getDaysOfLife() {
-		return daysOfLife;
-	}
-
-	public void setDaysOfLife(int daysOfLife) {
-		this.daysOfLife = daysOfLife;
-	}
-
-	public Chrysanthemum(String group, String sort, String color, double price, int daysOfLife) {
+	public Chrysanthemum(String group, String sort, String color, double price) {
 		super(group, sort, color, price);
-		this.daysOfLife = daysOfLife;
 	}
 }
