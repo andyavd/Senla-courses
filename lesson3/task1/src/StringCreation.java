7a2db8f0815f6b9ae35d535b1586bdfd698ce640
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-18 17:48:07 +0300

    Add files via upload

---
+++ lesson3/task1/src/StringCreation.java
@@ -0,0 +1,24 @@
+package task1;
+
+import java.util.Arrays;
+
+public class StringCreation {
+
+	public void getArrayAsString(int[] array) {
+		String createStringFromArray = Arrays.toString(array);
+		System.out.println("Array as a String is: " + createStringFromArray);
+	}
+
+	public void getSumAsString(int[] array) {
+
+		int sum = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			sum = sum + array[i];
+		}
+
+		String createStringFromSum = Integer.toString(sum);
+		System.out.println("Sum as a String is: " + createStringFromSum);
+	}
+
+}
---
+++ lesson3/task1/src/Test.java
@@ -0,0 +1,14 @@
+package task1;
+
+public class Test {
+
+	public static void main(String args[]) {
+
+		int[] array = { 4, 5, 7, 8, 87 };
+
+		StringCreation sc = new StringCreation();
+
+		sc.getArrayAsString(array);
+		sc.getSumAsString(array);
+	}
+}
