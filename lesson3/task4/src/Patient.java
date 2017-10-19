d764b91c2de80be4e4ddcb5e3568e0f5a797c3c4
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-20 01:45:21 +0300

    Update Patient.java

--- lesson3/task4/src/Patient.java
+++ lesson3/task4/src/Patient.java
@@ -1,21 +1,16 @@
 public class Patient extends APerson {
 
-	private String disease;
-	
-	public String getDisease() {
-		return disease;
-	}
-	public void setDisease(String disease) {
-		this.disease = disease;
-	}
-	
-	public Patient(String firstName, String lastName, String disease) {
+	public Patient(String firstName, String lastName) {
 		super(firstName, lastName);
-		this.disease = disease;
 	}
 	
 	@Override
 	public String toString() {
-		return "Patient " + firstName + " " + lastName + ", diagnosis - " + disease + ", ";
+		StringBuilder s = new StringBuilder();
+		s.append("Patient ");
+		s.append(firstName);
+		s.append(" ");
+		s.append(lastName);
+		return s.toString();
 	}
 }
