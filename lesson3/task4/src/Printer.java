f89979611d6e759ae55d3ed09f127fa05d1cc915
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-20 01:46:33 +0300

    Update Printer.java

--- lesson3/task4/src/Printer.java
+++ lesson3/task4/src/Printer.java
@@ -1,20 +1,6 @@
 public class Printer {
-	Hospital hospital = new Hospital();
 
-	public void getResults(Doctor doctor) {
-		System.out.println(doctor.toString() + doctor.isAnyoneLeft());
-	}
-
-	public void getProcess(Doctor doctor) {
-		System.out.println(doctor.toString() + " works today with " + doctor.getTotalDoctorsPatients()
-				+ doctor.changeTheWordPatient());
-	}
-
-	public void getDoctors(int totalDoctors) {
-		System.out.println("Today are working " + totalDoctors + " doctors.");
-	}
-
-	public void getPatients(int totalPatients) {
-		System.out.println("Today are visiting " + totalPatients + " patients.");
+	public static void print(String message) {
+		System.out.println(message);
 	}
 }
