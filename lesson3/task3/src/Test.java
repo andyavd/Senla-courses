e527949553c5e6c7dff6158c7b6dd8fb0a1598ae
Andrei Audzeichyk <andyavd@yandex.ru>
2017-10-20 01:38:48 +0300

    Update Test.java

--- lesson3/task3/src/Test.java
+++ lesson3/task3/src/Test.java
@@ -2,11 +2,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		Flower rose1 = new Rose("Rose", "Red", 3.1);
-		Flower rose2 = new Rose("Rose", "White", 3.3);
-		Flower chrysanthemum = new Chrysanthemum("Chrysanthemum", "Peach", 2.5);
-		Flower alstroemeria = new Alstroemeria("Alstroemeria", "Purple", 2.9);
-		Flower lilly = new Lilly("Lilly", "Cream", 4.1);
+		Flower rose1 = new Rose("Rose", 3.1);
+		Flower rose2 = new Rose("Rose", 3.3);
+		Flower chrysanthemum = new Chrysanthemum("Chrysanthemum", 2.5);
+		Flower alstroemeria = new Alstroemeria("Alstroemeria", 2.9);
+		Flower lilly = new Lilly("Lilly", 4.1);
 		
 		Flower[] bouquet = new Flower[7];
 		
