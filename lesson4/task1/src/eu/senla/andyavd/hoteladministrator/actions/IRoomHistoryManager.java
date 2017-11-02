7c4cfb2ae546cd1863636d8f9ce01f4591b9211f
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-02 06:23:10 +0200

    Delete IRoomHistoryManager.java

--- lesson4/task1/src/eu/senla/andyavd/hoteladministrator/actions/IRoomHistoryManager.java
+++
@@ -1,8 +0,0 @@
-package eu.senla.andyavd.hoteladministrator.actions;
-
-import eu.senla.andyavd.hoteladministrator.entities.RoomHistory;
-
-public interface IRoomHistoryManager {
-	public void addHistory(RoomHistory history);
-	public void showHistories();
-}
