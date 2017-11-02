93b97f59f304df4d14c8614673eadbf5c1977133
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-02 06:41:19 +0200

    Update SortingRoomsByCapacity.java

--- lesson4/task1/src/eu/senla/andyavd/hoteladministrator/utils/sorters/roomsorters/SortingRoomsByCapacity.java
+++ lesson4/task1/src/eu/senla/andyavd/hoteladministrator/utils/sorters/roomsorters/SortingRoomsByCapacity.java
@@ -1,23 +1,19 @@
-package eu.senla.andyavd.hoteladministrator.utils.sorters.roomsorters;
+package eu.senla.andyavd.hoteladministrator.utils.sorters.rooms;
 
 import java.util.Comparator;
 
 import eu.senla.andyavd.hoteladministrator.entities.Room;
 
-public class SortingRoomsByCapacity implements Comparator<Object>{
+public class SortingRoomsByCapacity implements Comparator<Room>{
 
 	@Override
-	public int compare(Object obj1, Object obj2) {
-
-		Room s1 = (Room)obj1;
-		Room s2 = (Room)obj2;
-
-		if (s1.getCapasity() > s2.getCapasity()) {
+	public int compare(Room r1, Room r2) {
+		if(r1 != null && r2 != null) {
+			return r1.getCapasity().compareTo(r2.getCapasity());	
+		} else if (r1 != null && r2 == null) {
 			return 1;
-		} else if (s1.getCapasity() < s2.getCapasity()) {
-			return -1;
 		} else {
-			return 0;
-		}
+			return -1;
+		}		
 	}
 }
