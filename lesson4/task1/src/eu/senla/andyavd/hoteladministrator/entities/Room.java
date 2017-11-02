58a51ed0ff743ea0d049c6e7581a5c5ad6f6ef6d
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-02 06:27:48 +0200

    Update Room.java

--- lesson4/task1/src/eu/senla/andyavd/hoteladministrator/entities/Room.java
+++ lesson4/task1/src/eu/senla/andyavd/hoteladministrator/entities/Room.java
@@ -1,7 +1,5 @@
 package eu.senla.andyavd.hoteladministrator.entities;
 
-import java.time.LocalDate;
-
 import eu.senla.andyavd.hoteladministrator.enums.RoomStars;
 import eu.senla.andyavd.hoteladministrator.enums.RoomStatus;
 
@@ -9,15 +7,12 @@ public class Room extends Entity{
 
 	private int id;
 	private int roomNumber;
-	private int capacity;
-	private double dailyPrice;
+	private Integer capacity;
+	private Double dailyPrice;
 	private RoomStars stars;
 	private RoomStatus status;
 	private RoomHistory[] histories = new RoomHistory[10];
-//	private int totalRoomVisitors = 0;
-//	private Visitor[] latestVisitors;
-	private LocalDate checkInDate;
-	private LocalDate checkOutDate;
+
 
 //	@Override
 	public int getId() {
@@ -37,19 +32,19 @@ public class Room extends Entity{
 		this.roomNumber = roomNumber;
 	}
 
-	public int getCapasity() {
+	public Integer getCapasity() {
 		return capacity;
 	}
 
-	public void setCapasity(int capacity) {
+	public void setCapasity(Integer capacity) {
 		this.capacity = capacity;
 	}
 
-	public double getDailyPrice() {
+	public Double getDailyPrice() {
 		return dailyPrice;
 	}
 
-	public void setDailyPrice(double dailyPrice) {
+	public void setDailyPrice(Double dailyPrice) {
 		this.dailyPrice = dailyPrice;
 	}
 
@@ -76,66 +71,21 @@ public class Room extends Entity{
 	public void setHistories(RoomHistory[] histories) {
 		this.histories = histories;
 	}
-	
-	public LocalDate getCheckInDate() {
-		return checkInDate;
-	}
-
-	public void setCheckInDate(LocalDate checkInDate) {
-		this.checkInDate = checkInDate;
-	}
-
-	public LocalDate getCheckOutDate() {
-		return checkOutDate;
-	}
-
-	public void setCheckOutDate(LocalDate checkOutDate) {
-		this.checkOutDate = checkOutDate;
-	}
-	
-//	public int getTotalRoomVisitors() {
-//		return totalRoomVisitors;
-//	}
-//
-//	public void setTotalRoomVisitors(int totalRoomVisitors) {
-//		this.totalRoomVisitors = totalRoomVisitors;
-//	}
-//	
-//	public Visitor[] getVisitors() {
-//		return visitors;
-//	}
-//
-//	public void setVisitors(Visitor[] visitors) {
-//		this.visitors = visitors;
-//	}
-//	
-//	public Visitor[] getLatestVisitors() {
-//		return latestVisitors;
-//	}
-//
-//	public void setLatestVisitors(Visitor[] latestVisitors) {
-//		this.latestVisitors = latestVisitors;
-//	}
-	
-
-	// Constructor without visitors
-	public Room(int roomNumber, int capacity, double dailyPrice, RoomStars stars, RoomStatus status) {
+		
+	public Room(int roomNumber, Integer capacity, Double dailyPrice, RoomStars stars, RoomStatus status) {
 		this.roomNumber = roomNumber;
 		this.capacity = capacity;
 		this.dailyPrice = dailyPrice;
 		this.stars = stars;
 		this.status = status;
 	}
-
-	// Constructor with visitors
-//	public Room(int roomNumber, int capacity, double dailyPrice, RoomStars stars, RoomStatus status, Visitor[] visitors) {
-//		this.roomNumber = roomNumber;
-//		this.capacity = capacity;
-//		this.dailyPrice = dailyPrice;
-//		this.stars = stars;
-//		this.status = status;
-//		this.visitors = visitors;
-//	}
+	
+	public Room(int id, int roomNumber, Integer capacity, Double dailyPrice) {
+		this.id = id;
+		this.roomNumber = roomNumber;
+		this.capacity = capacity;
+		this.dailyPrice = dailyPrice;
+	}
 
 	@Override
 	public String toString() {
@@ -152,12 +102,19 @@ public class Room extends Entity{
 		
 		if (status == RoomStatus.OCCUPIED) {
 			s.append("Room is occupied. ");
-//			s.append(visitors[0].toString());
 		} else if (status == RoomStatus.SERVICED){
 			s.append("Room is being serviced.");
 		} else {
 			s.append("Room is empty.");
 		}
 		return s.toString();
+	}
+	
+	public String getRoomParameters() {
+		return String.valueOf(new StringBuilder().append(id)
+				.append(" ").append(roomNumber)
+				.append(" ").append(capacity)
+				.append(" ").append(stars)
+				.append(" ").append(dailyPrice));
 	}
 }
