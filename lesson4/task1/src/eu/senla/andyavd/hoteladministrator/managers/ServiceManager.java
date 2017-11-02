f9e728ce4bc67f32d27d6c2ad856053b4ad1b679
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-02 06:36:48 +0200

    Update ServiceManager.java

--- lesson4/task1/src/eu/senla/andyavd/hoteladministrator/managers/ServiceManager.java
+++ lesson4/task1/src/eu/senla/andyavd/hoteladministrator/managers/ServiceManager.java
@@ -1,17 +1,17 @@
 package eu.senla.andyavd.hoteladministrator.managers;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-import eu.senla.andyavd.hoteladministrator.actions.IServiceManager;
+import eu.senla.andyavd.hoteladministrator.api.IServiceManager;
 import eu.senla.andyavd.hoteladministrator.entities.Service;
 import eu.senla.andyavd.hoteladministrator.storages.ServicesStorage;
-import eu.senla.andyavd.hoteladministrator.utils.Printer;
-import eu.senla.andyavd.hoteladministrator.utils.sorters.servisesorters.SortingServicesByName;
-import eu.senla.andyavd.hoteladministrator.utils.sorters.servisesorters.SortingServicesByPrice;
+import eu.senla.andyavd.hoteladministrator.utils.ArrayWorker;
 
 public class ServiceManager implements IServiceManager {
 
 	ServicesStorage ss = new ServicesStorage();
+	ArrayWorker aw = new ArrayWorker();
 
 	@Override
 	public void addService(Service service) {
@@ -19,29 +19,15 @@ public class ServiceManager implements IServiceManager {
 	}
 
 	@Override
-	public void showServices() {
-		for (int i = 0; i < ss.getServices().length; i++) {
-			if (ss.getServices()[i] != null) {
-				Printer.print(ss.getServices()[i].toString());
-			}
-		}
+	public Service[] getServices() {
+		return ss.getServices();
 	}
 
 	/* ======================sorting============================ */
-
+	
+	
 	@Override
-	public void sortServicesByName() {
-		Arrays.sort(ss.getServices(), new SortingServicesByName());
-		for (int i = 0; i < ss.getServices().length; i++) {
-			Printer.print(ss.getServices()[i].toString());
-		}
-	}
-
-	@Override
-	public void sortServicesByPrice() {
-		Arrays.sort(ss.getServices(), new SortingServicesByPrice());
-		for (int i = 0; i < ss.getServices().length; i++) {
-			Printer.print(ss.getServices()[i].toString());
-		}
+	public void sortServices(Comparator comparator) {
+		Arrays.sort(ss.getServices(), comparator);
 	}
 }
