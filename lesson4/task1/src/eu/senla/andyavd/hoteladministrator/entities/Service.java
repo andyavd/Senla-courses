4c75a10a79af9116105d21ba3fdcd0db54d983ff
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-03 16:42:23 +0200

    Update Service.java

--- lesson4/task1/src/eu/senla/andyavd/hoteladministrator/entities/Service.java
+++ lesson4/task1/src/eu/senla/andyavd/hoteladministrator/entities/Service.java
@@ -1,43 +1,46 @@
 package eu.senla.andyavd.hoteladministrator.entities;
 
-public class Service extends Entity{
-	
-	private int id=0;
+public class Service extends Entity {
+
+	private int id = 0;
 	private String name;
 	private Double dailyPrice;
-	
+
 	@Override
 	protected int getId() {
 		return id;
 	}
-	
-	@Override	
+
+	@Override
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Double getDailyPrice() {
 		return dailyPrice;
 	}
+
 	public void setDailyPrice(Double dailyPrice) {
 		this.dailyPrice = dailyPrice;
 	}
-	
+
 	public Service() {
-		
+
 	}
-	
+
 	public Service(String name, Double dailyPrice) {
 		this.name = name;
 		this.dailyPrice = dailyPrice;
 	}
-	
+
 	public Service(Integer id, String name, double dailyPrice) {
 		this.id = id;
 		this.name = name;
@@ -54,10 +57,9 @@ public class Service extends Entity{
 		s.append(" USD per day.)");
 		return s.toString();
 	}
-	
-	public String getServiceParameters() {
-		return String.valueOf(new StringBuilder().append(id)
-				.append(" ").append(name)
-				.append(" ").append(dailyPrice));
+
+	@Override
+	public String getEntityParameters() {
+		return String.valueOf(new StringBuilder().append(id).append(" ").append(name).append(" ").append(dailyPrice));
 	}
 }
