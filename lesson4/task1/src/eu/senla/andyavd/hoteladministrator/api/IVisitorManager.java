1a00d3cc2c8e6aea7c45982e91dbfa887edc87ea
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-03 16:39:58 +0200

    Update IVisitorManager.java

--- lesson4/task1/src/eu/senla/andyavd/hoteladministrator/api/IVisitorManager.java
+++ lesson4/task1/src/eu/senla/andyavd/hoteladministrator/api/IVisitorManager.java
@@ -8,12 +8,19 @@ import eu.senla.andyavd.hoteladministrator.entities.Visitor;
 
 public interface IVisitorManager {
 	public void addVisitor(Visitor visitor);
+
 	public void deleteVisitor(Visitor visitor);
+
 	public Visitor[] showVisitors();
+
 	public void updateVisitor(Visitor visitor, RoomHistory history);
+
 	public void addServicesToVisitor(Visitor visitor, Service service);
+
 	public Service[] showVisitorServices(Visitor visitor);
+
 	public void sortVisitorServicesByPrice(Visitor visitor, Comparator comparator);
-	
+
 	void sortVisitors(Comparator comparator);
+	
 }
