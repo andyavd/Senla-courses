7ae43d626a8e4fd1e67d79005f41bb634bdca771
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-03 16:44:13 +0200

    Update RoomHistoryManager.java

--- lesson4/task1/src/eu/senla/andyavd/hoteladministrator/managers/RoomHistoryManager.java
+++ lesson4/task1/src/eu/senla/andyavd/hoteladministrator/managers/RoomHistoryManager.java
@@ -3,12 +3,10 @@ package eu.senla.andyavd.hoteladministrator.managers;
 import eu.senla.andyavd.hoteladministrator.api.IRoomHistoryManager;
 import eu.senla.andyavd.hoteladministrator.entities.RoomHistory;
 import eu.senla.andyavd.hoteladministrator.storages.RoomHistoryStorage;
-import eu.senla.andyavd.hoteladministrator.utils.ArrayWorker;
 
 public class RoomHistoryManager implements IRoomHistoryManager {
 
 	public RoomHistoryStorage his = new RoomHistoryStorage();
-	ArrayWorker aw = new ArrayWorker();
 
 	@Override
 	public void addHistory(RoomHistory history) {
@@ -17,6 +15,6 @@ public class RoomHistoryManager implements IRoomHistoryManager {
 
 	@Override
 	public RoomHistory[] showHistories() {
-		return aw.getNotNullRoomHistories(his.getHistory());
+		return his.getHistory();
 	}
 }
