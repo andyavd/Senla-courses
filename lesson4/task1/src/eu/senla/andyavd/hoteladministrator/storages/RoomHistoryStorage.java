41cead371080ef11936273f18fe4a71a3b447e99
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-03 16:47:17 +0200

    Update RoomHistoryStorage.java

--- lesson4/task1/src/eu/senla/andyavd/hoteladministrator/storages/RoomHistoryStorage.java
+++ lesson4/task1/src/eu/senla/andyavd/hoteladministrator/storages/RoomHistoryStorage.java
@@ -1,5 +1,8 @@
 package eu.senla.andyavd.hoteladministrator.storages;
 
+import java.util.Arrays;
+
+import eu.senla.andyavd.hoteladministrator.entities.Entity;
 import eu.senla.andyavd.hoteladministrator.entities.RoomHistory;
 import eu.senla.andyavd.hoteladministrator.utils.ArrayWorker;
 
@@ -8,10 +11,15 @@ public class RoomHistoryStorage {
 	private RoomHistory[] histories = new RoomHistory[10];
 	private int counter = 0;
 
+	private RoomHistory[] castEntitiesArray(Entity[] entities) {
+		RoomHistory[] historyArray = Arrays.copyOf(entities, entities.length, RoomHistory[].class);
+		return historyArray;
+	}
+	
 	public void addHistory(RoomHistory history) {
 
 		 if (histories[histories.length-1] != null) {
-		 histories = (RoomHistory[]) ArrayWorker.expandRoomHistory(histories);
+		 histories = castEntitiesArray(ArrayWorker.expandArray(histories));
 		 }
 		histories[counter] = history;
 		histories[counter].setId(counter + 1);
