1c91d75e1f9dfd343b0aba2cb728399054a9f809
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-13 14:22:57 +0300

    Add files via upload

---
+++ lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/api/storages/IRoomHistoriesStorage.java
@@ -0,0 +1,13 @@
+package eu.senla.andyavd.hoteladministrator.api.storages;
+
+import java.util.List;
+
+import eu.senla.andyavd.hoteladministrator.entities.AEntity;
+import eu.senla.andyavd.hoteladministrator.entities.RoomHistory;
+
+public interface IRoomHistoriesStorage {
+
+	public void addHistory(RoomHistory history);
+
+	public List<AEntity> getHistory();
+}
