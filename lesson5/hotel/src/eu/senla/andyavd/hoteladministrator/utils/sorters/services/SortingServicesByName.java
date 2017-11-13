e8cf6b71bc5ffabb08718576446d81c8b080b0e1
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-13 14:37:37 +0300

    Add files via upload

---
+++ lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/sorters/services/SortingServicesByName.java
@@ -0,0 +1,20 @@
+package eu.senla.andyavd.hoteladministrator.utils.sorters.services;
+
+import java.util.Comparator;
+
+import eu.senla.andyavd.hoteladministrator.entities.AEntity;
+import eu.senla.andyavd.hoteladministrator.entities.Service;
+
+public class SortingServicesByName implements Comparator<AEntity>{
+
+	@Override
+	public int compare(AEntity s1, AEntity s2) {
+		if(s1 != null && s2 != null) {
+			return ((Service) s1).getName().compareTo(((Service) s2).getName());	
+		} else if (s1 != null && s2 == null) {
+			return 1;
+		} else {
+			return -1;
+		}		
+	}
+}
