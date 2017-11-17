6a8dfc251ec29e8c651e216ad40d987f9669515f
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-17 08:42:04 +0300

    Update SortingRoomsByStars.java

--- lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/sorters/rooms/SortingRoomsByStars.java
+++ lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/sorters/rooms/SortingRoomsByStars.java
@@ -2,13 +2,12 @@ package eu.senla.andyavd.hoteladministrator.utils.sorters.rooms;
 
 import java.util.Comparator;
 
-import eu.senla.andyavd.hoteladministrator.entities.AEntity;
 import eu.senla.andyavd.hoteladministrator.entities.Room;
 
-public class SortingRoomsByStars implements Comparator<AEntity>{
+public class SortingRoomsByStars implements Comparator<Room>{
 
 	@Override
-	public int compare(AEntity r1, AEntity r2) {
+	public int compare(Room r1, Room r2) {
 		if(r1 != null && r2 != null) {
 			return Integer.valueOf(((Room) r1).getStars().ordinal()).compareTo(Integer.valueOf(((Room) r2).getStars().ordinal()));	
 		} else if (r1 != null && r2 == null) {
