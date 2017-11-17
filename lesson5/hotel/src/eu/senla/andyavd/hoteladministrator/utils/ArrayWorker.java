ba7b174b657b227804616fdb709a82847e594091
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-17 08:44:08 +0300

    Update ArrayWorker.java

--- lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/ArrayWorker.java
+++ lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/ArrayWorker.java
@@ -1,26 +1,12 @@
 package eu.senla.andyavd.hoteladministrator.utils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import eu.senla.andyavd.hoteladministrator.entities.AEntity;
-import eu.senla.andyavd.hoteladministrator.entities.Room;
-import eu.senla.andyavd.hoteladministrator.enums.RoomStatus;
 
 public class ArrayWorker {
 
-	public static List<AEntity> getNotNullEmptyRooms(List<AEntity> entities) {
-
-		List<AEntity> newEntity = new ArrayList<AEntity>();
-
-		for (int i = 0; i < entities.size(); i++) {
-			if (((Room) entities.get(i)).getStatus() == RoomStatus.EMPTY)
-				newEntity.add(entities.get(i));
-		}
-		return newEntity;
-	}
-
-	public static String[] arrayToString(List<AEntity> entities) {
+	public static String[] arrayToString(List<? extends AEntity> entities) {
 
 		String[] stringArray = new String[entities.size()];
 		             
