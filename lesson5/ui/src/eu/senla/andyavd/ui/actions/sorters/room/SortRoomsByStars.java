c00d62e1f4547ea6bd560a0b3f87e9bbd25c9bf6
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-17 09:00:48 +0300

    Add files via upload

---
+++ lesson5/ui/src/eu/senla/andyavd/ui/actions/sorters/room/SortRoomsByStars.java
@@ -0,0 +1,15 @@
+package eu.senla.andyavd.ui.actions.sorters.room;
+
+import eu.senla.andyavd.hoteladministrator.utils.Printer;
+import eu.senla.andyavd.hoteladministrator.view.HotelManager;
+import eu.senla.andyavd.ui.api.IAction;
+
+public class SortRoomsByStars implements IAction {
+
+	@Override
+	public void execute() {
+		
+		Printer.printList(HotelManager.getInstance().sortRoomsByStars());
+
+	}
+}
