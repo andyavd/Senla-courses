58c7ad6df7e26acf5032a6bd643d9a6c0cbcd0f1
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-17 08:43:24 +0300

    Update SortingVisitorsByName.java

--- lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/sorters/visitors/SortingVisitorsByName.java
+++ lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/sorters/visitors/SortingVisitorsByName.java
@@ -2,13 +2,12 @@ package eu.senla.andyavd.hoteladministrator.utils.sorters.visitors;
 
 import java.util.Comparator;
 
-import eu.senla.andyavd.hoteladministrator.entities.AEntity;
 import eu.senla.andyavd.hoteladministrator.entities.Visitor;
 
-public class SortingVisitorsByName implements Comparator<AEntity>{
+public class SortingVisitorsByName implements Comparator<Visitor>{
 
 	@Override
-	public int compare(AEntity v1, AEntity v2) {
+	public int compare(Visitor v1, Visitor v2) {
 		if(v1 != null && v2 != null) {
 			return ((Visitor) v1).getLastName().compareTo(((Visitor) v2).getLastName());	
 		} else if (v1 != null && v2 == null) {
