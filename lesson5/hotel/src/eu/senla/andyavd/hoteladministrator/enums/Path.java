1135de8b7eca26d9951eb492e02ff3324e989082
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-17 08:36:29 +0300

    Update Path.java

--- lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/enums/Path.java
+++ lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/enums/Path.java
@@ -1,10 +1,11 @@
 package eu.senla.andyavd.hoteladministrator.enums;
 
 public enum Path {
-	ROOM_STORAGE_PATH ("/Users/andreiaudzeichyk/eclipse-workspace/eu.senla.andyavd.hotel/data/room.txt"),
-	VISITOR_STORAGE_PATH("/Users/andreiaudzeichyk/eclipse-workspace/eu.senla.andyavd.hotel/data/visitor.txt"),
-	SERVICE_STORAGE_PATH("/Users/andreiaudzeichyk/eclipse-workspace/eu.senla.andyavd.hotel/data/service.txt"),
-	LOG_STORAGE_PATH("/Users/andreiaudzeichyk/eclipse-workspace/eu.senla.andyavd.hotel/data/log.txt");
+	
+	ROOM_STORAGE_PATH ("data/room.txt"),
+	VISITOR_STORAGE_PATH("data/visitor.txt"),
+	SERVICE_STORAGE_PATH("data/service.txt"),
+	LOG_STORAGE_PATH("data/log.txt");
 	
 	private String path;
 
