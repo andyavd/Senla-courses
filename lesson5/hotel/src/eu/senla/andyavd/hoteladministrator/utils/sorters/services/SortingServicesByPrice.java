1c31bc993b9f2b57b444867d0cff2701f7e7d3ec
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-17 08:42:52 +0300

    Update SortingServicesByPrice.java

--- lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/sorters/services/SortingServicesByPrice.java
+++ lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/utils/sorters/services/SortingServicesByPrice.java
@@ -2,13 +2,12 @@ package eu.senla.andyavd.hoteladministrator.utils.sorters.services;
 
 import java.util.Comparator;
 
-import eu.senla.andyavd.hoteladministrator.entities.AEntity;
 import eu.senla.andyavd.hoteladministrator.entities.Service;
 
-public class SortingServicesByPrice implements Comparator<AEntity>{
+public class SortingServicesByPrice implements Comparator<Service>{
 
 	@Override
-	public int compare(AEntity s1, AEntity s2) {
+	public int compare(Service s1, Service s2) {
 		if(s1 != null && s2 != null) {
 			return ((Service) s1).getDailyPrice().compareTo(((Service) s2).getDailyPrice());	
 		} else if (s1 != null && s2 == null) {
