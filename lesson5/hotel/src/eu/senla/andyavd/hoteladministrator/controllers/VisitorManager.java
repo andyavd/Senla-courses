ab291a2ee1236945609dc7b9cae2357682269564
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-21 03:42:32 +0300

    Update VisitorManager.java

--- lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/controllers/VisitorManager.java
+++ lesson5/hotel/src/eu/senla/andyavd/hoteladministrator/controllers/VisitorManager.java
@@ -1,8 +1,14 @@
 package eu.senla.andyavd.hoteladministrator.controllers;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 
 import eu.senla.andyavd.hoteladministrator.api.controllers.IVisitorManager;
 import eu.senla.andyavd.hoteladministrator.entities.RoomHistory;
@@ -12,12 +18,14 @@ import eu.senla.andyavd.hoteladministrator.enums.Path;
 import eu.senla.andyavd.hoteladministrator.storages.VisitorsStorage;
 import eu.senla.andyavd.hoteladministrator.utils.ArrayWorker;
 import eu.senla.andyavd.hoteladministrator.utils.FileReader;
-import eu.senla.andyavd.hoteladministrator.utils.FileWriter;
+import eu.senla.andyavd.hoteladministrator.utils.FileWriterSenla;
 
 public class VisitorManager implements IVisitorManager {
 	
+	final static Logger logger = Logger.getLogger(VisitorManager.class);
+	
 	FileReader fileReader = new FileReader();
-	FileWriter fileWriter = new FileWriter();
+	FileWriterSenla fileWriter = new FileWriterSenla();
 	
 	private final static String path = Path.VISITOR_STORAGE_PATH.getPath();
 
@@ -37,6 +45,21 @@ public class VisitorManager implements IVisitorManager {
 	}
 
 	@Override
+	public Integer getTotalVisitorsOnDate(LocalDate date) {
+		Integer count = 0;
+		List<Visitor> visitors = getVisitors();
+		for (int i = 0; i < visitors.size(); i++) {
+			Visitor visitor = visitors.get(i);
+			if (visitor != null && visitor.getHistory() != null) {
+				if (date.isAfter(visitor.getHistory().getCheckInDate()) && date.isBefore(visitor.getHistory().getCheckOutDate())) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+	
+	@Override
 	public void updateVisitor(Visitor visitor, RoomHistory history) {
 		VisitorsStorage.getInstance().updateVisitor(visitor, history);
 	}
@@ -48,7 +71,7 @@ public class VisitorManager implements IVisitorManager {
 			visitor.getHistory().getService().add(service);
 
 		} else {
-			throw new NullPointerException("No such checked-in user!");
+			logger.error("No such visitor to bill!");
 		}
 	}
 
@@ -75,12 +98,16 @@ public class VisitorManager implements IVisitorManager {
 	public Visitor getVisitorById(Integer id) {
 		return VisitorsStorage.getInstance().getVisitorById(id);
 	}
-
+	
 	@Override
 	public void saveToFile() {
-		fileWriter.writeToFile(ArrayWorker.arrayToString(VisitorsStorage.getInstance().getVisitors()), path);
+		try{
+			fileWriter.writeToFile(ArrayWorker.arrayToString(VisitorsStorage.getInstance().getVisitors()), path);
+		} catch (Exception e) {
+			logger.error("Failed to save to a file!", e);
+		}
 	}
-
+	
 	@Override
 	public String[] loadFromFile() {
 		return fileReader.readFromFile(VisitorManager.path);
