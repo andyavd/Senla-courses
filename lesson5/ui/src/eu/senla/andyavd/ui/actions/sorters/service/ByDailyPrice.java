cbfdcef91b7bac334bcebe373cd4e021a06fd9a6
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-24 21:21:47 +0300

    Update ByDailyPrice.java

--- lesson5/ui/src/eu/senla/andyavd/ui/actions/sorters/service/ByDailyPrice.java
+++ lesson5/ui/src/eu/senla/andyavd/ui/actions/sorters/service/ByDailyPrice.java
@@ -4,7 +4,7 @@ import eu.senla.andyavd.hoteladministrator.utils.Printer;
 import eu.senla.andyavd.hoteladministrator.view.HotelManager;
 import eu.senla.andyavd.ui.api.IAction;
 
-public class SortServicesByPrice implements IAction {
+public class ByDailyPrice implements IAction {
 
 	@Override
 	public void execute() {
