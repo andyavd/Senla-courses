4728c7b5d9f4ceb4a52f4a10f1d76f41beca7ec3
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-24 21:11:05 +0300

    Update CheckOutAction.java

--- lesson5/ui/src/eu/senla/andyavd/ui/actions/visitor/CheckOutAction.java
+++ lesson5/ui/src/eu/senla/andyavd/ui/actions/visitor/CheckOutAction.java
@@ -10,9 +10,9 @@ import eu.senla.andyavd.hoteladministrator.view.HotelManager;
 import eu.senla.andyavd.ui.api.IAction;
 import eu.senla.andyavd.ui.utils.InputReader;
 
-public class CheckOutVisitorAction implements IAction {
+public class CheckOutAction implements IAction {
 
-	final static Logger logger = Logger.getLogger(CheckOutVisitorAction.class);
+	final static Logger logger = Logger.getLogger(CheckOutAction.class);
 	
 	@Override
 	public void execute() {
