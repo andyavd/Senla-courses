bfab1fb91a65ce2e361d307257a0a1d23d5bb90a
Andrei Audzeichyk <andyavd@yandex.ru>
2017-11-25 10:25:22 +0300

    Update ByLastName.java

--- lesson5/ui/src/eu/senla/andyavd/ui/actions/sorters/visitor/ByLastName.java
+++ lesson5/ui/src/eu/senla/andyavd/ui/actions/sorters/visitor/ByLastName.java
@@ -4,7 +4,7 @@ import eu.senla.andyavd.hoteladministrator.utils.Printer;
 import eu.senla.andyavd.hoteladministrator.view.HotelManager;
 import eu.senla.andyavd.ui.api.IAction;
 
-public class SortVisitorsByName implements IAction {
+public class ByLastName implements IAction {
 
 	@Override
 	public void execute() {
