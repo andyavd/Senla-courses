6d5da99b322ac7d2cff6cc2a5cb11d38eb96b98d
Andrei Audzeichyk <andyavd@yandex.ru>
2018-02-04 18:25:53 +0300

    Add files via upload

---
+++ lesson10/task2/input.sql
@@ -0,0 +1,19 @@
+INSERT INTO hotel.service (service_name, daily_price) VALUES
+('Laundry',5),
+('Ironing',8),
+('Cleaning',10),
+('WakeUp', 1);
+
+INSERT INTO hotel.visitor (visitor_name) VALUES
+('Marsh'),
+('Broflovski'),
+('McCormick'),
+('Cartman'),
+('Stotch');
+
+INSERT INTO hotel.room (room_number, capacity, daily_price, stars, room_status) VALUES
+(101,2,12,'Standard','EMPTY'),
+(102,2,12,'Standard','EMPTY'),
+(103,2,15,'Lux','EMPTY'),
+(104,3,17,'Standard','EMPTY'),
+(105,4,17,'Juniour_Suite','EMPTY');
