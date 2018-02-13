893a29ee2f934a25d2e6e107c6b27a6a2c708f68
Andrei Audzeichyk <andyavd@yandex.ru>
2018-02-13 08:45:06 +0300

    Update hotel.sql

--- lesson10/task2/hotel.sql
+++ lesson10/task2/hotel.sql
@@ -9,15 +9,13 @@ CREATE TABLE IF NOT EXISTS hotel.room (
   capacity TINYINT(1) NOT NULL,
   daily_price DOUBLE NOT NULL,
   stars ENUM('Standard','Juniour_Suite','Lux','President_Lux') NOT NULL,
-  room_status ENUM('Empty','Occupied','Serviced') NOT NULL,
-  history_id TINYINT(3) NULL,
+  room_status ENUM('Empty','Occupied','Serviced') NOT NULL DEFAULT 'Empty',
   PRIMARY KEY (id)); 
 
 /*Creating Visitors table*/
 CREATE TABLE IF NOT EXISTS hotel.visitor (
   id TINYINT(3) NOT NULL AUTO_INCREMENT,
-  visitor_name VARCHAR(45) NOT NULL,
-  history_id TINYINT(3) NULL,
+  visitor_name VARCHAR(45) NOT NULL UNIQUE,
   PRIMARY KEY (id)); 
 
 /*Creating Services table*/
@@ -32,14 +30,21 @@ CREATE TABLE IF NOT EXISTS hotel.history (
   id TINYINT(3) NOT NULL AUTO_INCREMENT,
   room_id TINYINT(3) NOT NULL,
   visitor_id TINYINT(3) NOT NULL,
-  service_id TINYINT(3) NULL,
+  visitor_service_id TINYINT(3) NULL,
   check_in DATE NOT NULL,
   check_out DATE NOT NULL,
-  history_status ENUM('CheckIn','CheckOut') NOT NULL,
+  history_status ENUM('CheckIn','CheckOut') NOT NULL DEFAULT 'CheckOut',
   PRIMARY KEY (id),
   FOREIGN KEY (room_id) REFERENCES room (id),
-  FOREIGN KEY (visitor_id) REFERENCES visitor (id),
-  FOREIGN KEY (service_id) REFERENCES service (id));
+  FOREIGN KEY (visitor_id) REFERENCES visitor (id));
 
-ALTER TABLE room ADD FOREIGN KEY (history_id) REFERENCES history (id);
-ALTER TABLE visitor ADD FOREIGN KEY (history_id) REFERENCES history (id)
+/*Creating visitor_service table*/
+CREATE TABLE IF NOT EXISTS visitor_service (
+id TINYINT(3) NOT NULL AUTO_INCREMENT,
+history_id TINYINT(3) NOT NULL,
+service_id TINYINT(3) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (service_id) REFERENCES service(id),
+FOREIGN KEY (history_id) REFERENCES history(id));
+
+ALTER TABLE history ADD FOREIGN KEY (visitor_service_id) REFERENCES visitor_service (history_id);
